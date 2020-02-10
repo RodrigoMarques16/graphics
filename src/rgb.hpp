e53979a87c5b1d58be8a5607aac2dc66b55a65a5
Rodrigo Marques <rmarques1611@gmail.com>
2020-02-10 21:07:42 +0000

    add rgb type for type safety with vec3

---
+++ src/rgb.hpp
@@ -0,0 +1,68 @@
+#pragma once
+
+struct RGB {
+    int r, g, b;
+
+    // RGB() = default;
+    // constexpr RGB(float r, float g, float b) : r(r), g(g), b(b) {};
+
+    constexpr RGB operator+() const { return *this; }
+    constexpr RGB operator-() const { return RGB(-r, -g, -b); }
+
+    constexpr RGB operator+(const RGB& other) const {
+        return RGB{r + other.r, g + other.g, b + other.b};
+    }
+
+    RGB& operator+=(const RGB& other) {
+        r += other.r;
+        g += other.g;
+        b += other.b;
+        return *this;
+    }
+
+    constexpr RGB operator-(const RGB& other) const {
+        return RGB{r - other.r, g - other.g, b - other.b};
+    }
+
+    RGB& operator-=(const RGB& other) {
+        r -= other.r;
+        g -= other.g;
+        b -= other.b;
+        return *this;
+    }
+
+    constexpr RGB operator*(float f) const {
+        return RGB{r * f, g * f, b * f};
+    }
+
+    constexpr friend RGB operator*(float f, const RGB v) {
+        return RGB{v.r * f, v.g * f, v.b * f};
+    }
+
+    RGB& operator*=(float f) {
+        r *= f;
+        g *= f;
+        b *= f;
+        return *this;
+    }
+
+    constexpr RGB operator/(float f) { return RGB{r / f, g / f, b / f}; }
+
+    RGB& operator/=(float f) {
+        r /= f;
+        g /= f;
+        b /= f;
+        return *this;
+    }
+
+    constexpr RGB operator*(const RGB& other) const {
+        return RGB{r * other.r, g * other.g, b * other.b};
+    }
+
+    RGB& operator*=(const RGB& other) {
+        r *= other.r;
+        g *= other.g;
+        b *= other.b;
+        return *this;
+    }
+};
