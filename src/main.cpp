7af3d925e9a0f45674fb06f3bb226c9926e17644
Rodrigo Marques <rodrigocanedomarques@hotmail.com>
2020-02-18 09:37:19 +0000

    main

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,7 @@
-#include "math/vec3.hpp"
+#include "math/onb.hpp"
 #include "math/rgb.hpp"
 #include "math/unit3.hpp"
-#include "math/onb.hpp"
+#include "math/vec3.hpp"
+#include <iostream>
 
-int main() {
-    static_assert(std::is_same<std::is_same<int, char>::value_type, bool>::value);
-}
+int main() {}
