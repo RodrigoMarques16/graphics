38215c843f89e4edfcd2fd9d085dd724f7e0c4f4
Rodrigo Marques <rodrigocanedomarques@hotmail.com>
2020-02-18 09:37:39 +0000

    add orthonormal basis library

--- src/math/onb.hpp
+++ src/math/onb.hpp
@@ -1,22 +1,79 @@
 #pragma once
 
-#include "vec3.hpp"
 #include <ostream>
+#include "unit3.hpp"
 
 struct ONB {
-    static constexpr const float eps = 0.00000001;
+    static constexpr const float eps = 0.01f;
 
-    Vec3 u, v, w;
+    Unit3 u, v, w;
 
-    static constexpr void fromU(const Vec3& xx);
-    static constexpr void fromV(const Vec3& vec);
-    static constexpr void fromW(const Vec3& vec);
-    static constexpr void fromUV(const Vec3& vec, const Vec3& b);
-    static constexpr void fromVU(const Vec3& vec, const Vec3& b);
-    static constexpr void fromUW(const Vec3& vec, const Vec3& b);
-    static constexpr void fromWU(const Vec3& vec, const Vec3& b);
-    static constexpr void fromVW(const Vec3& vec, const Vec3& b);
-    static constexpr void fromWV(const Vec3& vec, const Vec3& b);
+    static ONB fromU(const Unit3& xx) {
+        auto yy = xx.cross(Unit3::xAxis);
+        if (yy.lengthSquared() < eps)
+            yy = xx.cross(Unit3::yAxis);
+        auto zz = xx.cross(yy);
+        return { xx, yy.unitVector(), zz.unitVector() };
+    }
+
+    static ONB fromV(const Unit3& yy) {
+        auto xx = yy.cross(Unit3::xAxis);
+        if (xx.lengthSquared() < eps)
+            xx = yy.cross(Unit3::yAxis);
+        auto zz = xx.cross(yy);
+        return {xx.unitVector(), yy, zz.unitVector()};
+    }
+
+    static ONB fromW(const Unit3& zz) {
+        auto xx = zz.cross(Unit3::xAxis);
+        if (xx.lengthSquared() < eps)
+            xx = zz.cross(Unit3::yAxis);
+        auto yy = xx.cross(zz);
+        return {xx.unitVector(), yy.unitVector(), zz};
+    }
+
+    static ONB fromUV(const Unit3& xx, const Unit3& u) {
+        auto zz = xx.cross(u).unitVector();
+        auto yy = zz.cross(xx).unitVector();
+        return {xx, yy, zz};
+    }
+
+    static ONB fromVU(const Unit3& yy, const Unit3& u) {
+        auto zz = u.cross(yy).unitVector();
+        auto xx = yy.cross(zz).unitVector();
+        return {xx, yy, zz};
+    }
+
+    static ONB fromUW(const Unit3& xx, const Unit3& w) {
+        auto yy = w.cross(xx).unitVector();
+        auto zz = xx.cross(yy).unitVector();
+        return {xx, yy, zz};
+    }
+    
+    static ONB fromWU(const Unit3& zz, const Unit3& u) {
+        auto yy = zz.cross(u).unitVector();
+        auto xx = yy.cross(zz).unitVector();
+        return {xx, yy, zz};
+    }
+
+    static ONB fromVW(const Unit3& yy, const Unit3& w) {
+        auto xx = yy.cross(w).unitVector();
+        auto zz = xx.cross(yy).unitVector();
+        return {xx, yy, zz};
+    }
+
+    static ONB fromWV(const Unit3& zz, const Unit3& v) {
+        auto xx = v.cross(zz).unitVector();
+        auto yy = zz.cross(xx).unitVector();
+        return {xx, yy, zz};
+    }
+
+    bool operator==(const ONB& other) {
+        return u == other.u
+            && v == other.v
+            && w == other.w;
+    }
+
 };
 
 std::ostream& operator<<(std::ostream& os, const ONB& o);
