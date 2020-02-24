cf401820706a590fd75baa61bc3df945e914f8f9
Rodrigo Marques <rmarques1611@gmail.com>
2020-02-24 22:31:42 +0000

    prints a pretty square (4 triangles)

--- src/main.cpp
+++ src/main.cpp
@@ -11,14 +11,18 @@
 #include <stdio.h>
 
 int main() {
-    Sphere s = {{250, 250, -1000}, 150*150};
-    Triangle t = {{300, 600, -800}, {0, 100, -1000}, {450, 20, -1000}};
+    // Sphere s = {{250, 250, -1000}, 150*150};
+    // Triangle t = {{300, 600, -800}, {0, 100, -1000}, {450, 20, -1000}};
 
     Image img(500, 500, {.2, .2, .2});
 
+    // scene.addSphere(s);
     Scene scene;
-    scene.addSphere(s);
-    scene.addTriangle(t);
+    //                     BLUE                RED               GREEN
+    scene.addTriangle({  0, 500, -1000}, {250, 250, -1000}, {500, 500, -1000}); // top
+    scene.addTriangle({  0, 500, -1000}, {250, 250, -1000}, {0,     0, -1000}); // left
+    scene.addTriangle({500,   0, -1000}, {250, 250, -1000}, {500, 500, -1000}); // right
+    scene.addTriangle({500,   0, -1000}, {250, 250, -1000}, {0,     0, -1000}); // bottom
 
     for (float i = 0; i < 500; i++) {
         for (float j = 0; j < 500; j++) {
